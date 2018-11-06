777231ac5871bc326175319fba7e938d8d7329da
Joshua Alvarado <alvaradojoshua0@gmail.com>
2018-11-05 21:44:38 -0800

    Adhere to Array Literal

--- Sources/SwiftJSONType/SwiftJSONTypeExpressibleByLiteralExtension.swift
+++ Sources/SwiftJSONType/SwiftJSONTypeExpressibleByLiteralExtension.swift
@@ -5,3 +5,12 @@
 //  Created by Joshua Alvarado on 11/4/18.
 //
 
+
+extension JSONType: ExpressibleByArrayLiteral {
+    public typealias ArrayLiteralElement = Encodable
+
+    public init(arrayLiteral elements: Encodable...) {
+        self.init(elements as? JSONType.JSONValue)
+    }
+}
+
--- Tests/SwiftJSONTypeTests/SwiftJSONTypeTests.swift
+++ Tests/SwiftJSONTypeTests/SwiftJSONTypeTests.swift
@@ -1,15 +1,21 @@
 import XCTest
+
 @testable import SwiftJSONType
 
 final class SwiftJSONTypeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertNil(JSONType(nil).value)
+    func testJSONTypeExpressibleByArrayLiteral() {
+        let jsonType = JSONType(value: [1])
+        XCTAssertNotNil(jsonType.value)
+        XCTAssertEqual(jsonType.value as? [Int], [1])
+
+        let array: [JSONType] = [1]
+        let values = array.map({$0.value as? Int})
+        XCTAssertEqual(values, [1])
+    }
+
     }
 
     static var allTests = [
-        ("testExample", testExample),
-    ]
+        ("testJSONTypeExpressibleByArrayLiteral", testJSONTypeExpressibleByArrayLiteral),
+        ]
 }
