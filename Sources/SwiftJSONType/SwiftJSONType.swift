ed3ef0e7f1fce7cda7e1de63e0233d0c1f82602a
Joshua Alvarado <jalvarado@fb.com>
2018-11-29 01:23:27 -0800

    Move Encodable to extension

--- Sources/SwiftJSONType/SwiftJSONType.swift
+++ Sources/SwiftJSONType/SwiftJSONType.swift
@@ -1,4 +1,4 @@
-public struct JSONType: Encodable {
+public struct JSONType {
     public typealias JSONValue = Encodable
 
     public let value: JSONValue?
@@ -11,6 +11,8 @@ public struct JSONType: Encodable {
         self.value = value
     }
 
+// MARK: - Encodable
+extension JSONType: Encodable {
     public func encode(to encoder: Encoder) throws {
         try value?.encode(to: encoder)
     }
